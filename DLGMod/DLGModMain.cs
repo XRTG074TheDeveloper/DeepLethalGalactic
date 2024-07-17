c4b3e0df33c1d28bf45e097bffc764e2351a0e10
Pinecone's Crew <166554820+XRTG074TheDeveloper@users.noreply.github.com>
2024-07-17 21:19:18 +0200

    Added Harmony instance initialization

--- DLGMod/DLGModMain.cs
+++ DLGMod/DLGModMain.cs
@@ -1,7 +1,13 @@
-﻿namespace DLGMod
-{
-    public class DLGModMain
-    {
+﻿using BepInEx;
+using HarmonyLib;
 
+namespace DLGMod
+{
+    [BepInPlugin(GUID, "Deep Lethal Company", "0.0.0.1")]
+    public class DLGModMain : BaseUnityPlugin
+    {
+        internal const string GUID = "XRTG074TheDeveloper.DeepLethalGalactic";
+
+        internal readonly Harmony harmonyInstance = new Harmony(GUID);
     }
 }
